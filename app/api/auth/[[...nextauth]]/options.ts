7d155b366c348e452fb5044454be1dcb043cb4cf
hungtp1996nd <hungtp1996nd@gmail.com>
2023-07-09 22:30:51 +0700

    add fb provider and env to vercel

--- app/api/auth/[[...nextauth]]/options.ts
+++ app/api/auth/[[...nextauth]]/options.ts
@@ -1,12 +1,17 @@
 import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
+import FacebookProvider from "next-auth/providers/facebook";
 
 export const options: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID ?? "",
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET ?? "",
     }),
     CredentialsProvider({
       name: "Credentials",
